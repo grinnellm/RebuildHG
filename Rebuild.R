14acde8a7fc27ce2eb075b6a19c822f8ec05185b
Matthew Grinnell <matthew.grinnell@dfo-mpo.gc.ca>
2020-04-22 14:23:36 -0700

    Add spawn by spawn number

--- Rebuild.R
+++ Rebuild.R
@@ -658,7 +658,7 @@ timingPlot <- ggplot(data = filter(siAllLong, !is.na(Survey)), aes(x = Year)) +
 
 # Spawn duration by year and spatial unit
 durationPlot <- ggplot(data = siAll, mapping = aes(y = Duration, x = Year)) +
-  geom_point(mapping = aes(shape = Survey), na.rm = TRUE) +
+  geom_point(mapping = aes(shape = Survey), alpha = 0.5, na.rm = TRUE) +
   # geom_boxplot(mapping = aes(group=Year), na.rm = TRUE) +
   geom_vline(xintercept = newSurvYr - 0.5, linetype = "dashed", size = 0.25) +
   scale_x_continuous(breaks = seq(from = 1000, to = 3000, by = 10)) +
@@ -684,6 +684,22 @@ numSpawnPlot <- ggplot(data = siAll, mapping = aes(x = Year)) +
   theme(legend.position = "top") +
   ggsave(
     filename = file.path(region, "SpawnNumber.png"),
+    height = min(8.75, n_distinct(siAll$SpUnit) * 1.9 + 1), width = figWidth
+  )
+
+# Spawn index by year and spatial unit
+siPlot <- ggplot(data = siAll, mapping = aes(y = SITotal, x = Year)) +
+  geom_point(mapping = aes(shape = Survey), alpha = 0.5, na.rm = TRUE) +
+  geom_vline(xintercept = newSurvYr - 0.5, linetype = "dashed", size = 0.25) +
+  scale_x_continuous(breaks = seq(from = 1000, to = 3000, by = 10)) +
+  scale_y_continuous(labels = comma) +
+  expand_limits(x = yrRange, y = 0) +
+  labs(y = "Spawn index (t)") +
+  facet_grid(SpUnit ~ .) +
+  myTheme +
+  theme(legend.position = "top") +
+  ggsave(
+    filename = file.path(region, "SpawnIndex.png"),
     height = min(8.75, n_distinct(siAll$SpUnit) * 1.9 + 1), width = figWidth
   )
 
